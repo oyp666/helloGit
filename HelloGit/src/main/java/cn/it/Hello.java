0d492f7e63acad2dde5441c31dc1236e2a746eec
wufeng666 <1945130452@qq.com>
2020-07-10 17:00:25 +0800

    git第一次提交

---
+++ HelloGit/src/main/java/cn/it/Hello.java
@@ -0,0 +1,13 @@
+package cn.it;
+
+/*
+ *   @author oyp
+ *   @data 2020/7/10 16:31
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
